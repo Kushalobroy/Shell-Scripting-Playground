dfb1d21c38455ee2cb5507e74b7d62613635fab5
Kushal <kushalobroy@gmail.com>
2024-09-23 15:40:18 +0530

    initial commit

---
+++ Sport.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Sport script - uses parameters
+
+echo "Enter Your Name: "
+read NAME
+echo "Enter Sport which you like most: "
+read SPORT
+
+# Prints the name and SPORT
+
+echo $NAME likes to watch $SPORT.
+
+# Pause the script before exit
+echo "Press Enter to exit..."
+read  # This will keep the terminal open until you press Enter
